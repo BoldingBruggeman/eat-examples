c73287d52c38b2361fe7d1eda6aaf83e9208a8bd
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-05-26 10:43:05 +0100

    corrected run scripts

--- north_sea_pisces/assimilate_sst.py
+++ north_sea_pisces/assimilate_sst.py
@@ -7,6 +7,7 @@ filter = eatpy.PDAF(eatpy.pdaf.FilterType.ESTKF)
 experiment.add_plugin(eatpy.plugins.select.Select(include=("temp", "salt")))
 experiment.add_plugin(eatpy.plugins.check.Finite())
 
+# If you comment out the line below, you run the ensemble only, without assimilation
 experiment.add_observations("temp[-1]", "cci_sst.dat")
 
 experiment.run(filter)
--- north_sea_pisces/assimilate_sst_chl.py
+++ north_sea_pisces/assimilate_sst_chl.py
@@ -19,11 +19,9 @@ experiment.add_plugin(
         minimum=1e-12
     )
 )
-experiment.add_plugin(eatpy.plugins.output.NetCDFStats('ensemble_stats.nc'))
 
-#experiment.add_observations("temp[-1]", "cci_sst.dat")
-#experiment.add_observations("total_chlorophyll_calculator_result[-1]", "cci_chl.dat")
-import datetime
-experiment.add_dummy_observations([(datetime.datetime(2020,1,2)+datetime.timedelta(days=i)) for i in range(3*365)])
+# If you comment out the two lines below, you run the ensemble only without assimilation
+experiment.add_observations("temp[-1]", "cci_sst.dat")
+experiment.add_observations("total_chlorophyll_calculator_result[-1]", "cci_chl.dat")
 
 experiment.run(filter)
